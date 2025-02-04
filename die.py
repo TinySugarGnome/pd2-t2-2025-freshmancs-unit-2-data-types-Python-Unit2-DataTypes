f3d406ff41a0066bcd41aaf0eb1f7df7c7bea6f6
TinySugarGnome <nelsonouyang10@gmail.com>
2025-02-04 09:25:15 -0500

    hi

--- die.py
+++ die.py
@@ -1,8 +1,14 @@
-#list
+""" #list
 values = [1,2.23,5,7,2,30,15]
 for i in values:
     print(i)
 
 #specific thingy
 values = [1, 2.23, 5, 7, 2, 30, 15]
-print(values[5])
+print(values[5]) """
+
+
+sentence = input("say any sentence")
+x = sentence
+y= x.split()
+print(y)
