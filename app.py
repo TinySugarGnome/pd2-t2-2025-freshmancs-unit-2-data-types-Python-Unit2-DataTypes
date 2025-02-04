c054d95b58a13aff5726fb95656b2fa604041db2
TinySugarGnome <nelsonouyang10@gmail.com>
2025-02-04 09:19:57 -0500

    yea

---
+++ app.py
@@ -0,0 +1,9 @@
+name = "Nelson" #strings represent names, word, etc
+age = 15 #integers for WHOLE nums
+graduated = False #boolean = true/false , typically ysed for evaluations
+tall = True
+money = 10000.15 #floats for decimals14
+
+bill = float(input("how bill"))
+total = 15 + float(bill)
+print(total)
---
+++ die.py
@@ -0,0 +1,8 @@
+#list
+values = [1,2.23,5,7,2,30,15]
+for i in values:
+    print(i)
+
+#specific thingy
+values = [1, 2.23, 5, 7, 2, 30, 15]
+print(values[5])
